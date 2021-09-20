a057847cd7c237846a0943b1e3efb22b95a720cf
agustinus-robert <agustinus2h@gmail.com>
2021-09-20 08:55:31 +0700

    views dan controller maps

---
+++ application/controllers/Opmaps.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+    
+    public function index(){
+        $this->load->view('header');
+        $this->load->view('mpv/index');
+        $this->load->view('footer');
+    }
+}
