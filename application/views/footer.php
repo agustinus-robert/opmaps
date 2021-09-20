c3a3aaeb9f4c50673922055372803705ec1a17a7
agustinus-robert <agustinus2h@gmail.com>
2021-09-20 09:22:22 +0700

    perbaikan height style maps

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,3 +1,7 @@
- </div>
+        </div>
+    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+  integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+  crossorigin=""></script>
+  <script type = 'text/javascript' src = "<?php echo base_url(); ?>asset/js/maps_config.js"></script>
     </body>
 </html>
--- application/views/header.php
+++ application/views/header.php
@@ -5,12 +5,10 @@
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
   integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
   crossorigin=""/>
-<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
-  integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
-  crossorigin=""></script>
+
   
     <link rel = "stylesheet" type = "text/css" href = "<?php echo base_url(); ?>asset/css/style.css"> 
-      <script type = 'text/javascript' src = "<?php echo base_url(); ?>asset/js/maps_config.js"></script> 
+       
          
         <title>Maps</title>
     </head>
