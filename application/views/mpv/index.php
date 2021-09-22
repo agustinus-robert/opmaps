6a44b5238d666a2e63850ef405169863bd8ca468
agustinus-robert <agustinus2h@gmail.com>
2021-09-22 08:12:16 +0700

    objek geocoding

--- application/views/mpv/index.php
+++ application/views/mpv/index.php
@@ -2,7 +2,7 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title">Modal title</h5>
+        <h5 class="modal-title">Objek GeoCoding</h5>
         
       </div>
       <div class="modal-body">
